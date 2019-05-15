02d37e3d2ae880d8008d05edcb5d5ad03a904214
Reza <rezashirkavand1997@gmail.com>
2019-05-15 18:37:20 +0430

    some refactoring

--- Grammar.py
+++ Grammar.py
@@ -4,7 +4,7 @@ from copy import deepcopy
 
 class Grammar:
     def __init__(self, prods):
-        self.none_terminals = []  # TODO
+        self.non_terminals = []  # TODO
         self.prods = prods
 
     def left_factorize_prods(self):
@@ -16,12 +16,12 @@ class Grammar:
     def remove_left_recursion(self):
         pass
 
-    def find_ebsilon_none_terminals(self):
-        ebsilon_none_terminals = []
+    def find_epsilon_none_terminals(self):
+        epsilon_none_terminals = []
         for prod in self.prods:
             for sub_prod in prod.sub_prods:
                 if len(sub_prod) == 1 and sub_prod[0] == "":
-                    ebsilon_none_terminals.append(prod.none_terminal)
+                    epsilon_none_terminals.append(prod.none_terminal)
                 else:
                     for term in sub_prod:
                         pass #TODO
@@ -37,10 +37,7 @@ class Grammar:
 
     @staticmethod
     def make_grammar(self, compressed_prods):
-
-
-        none_terminals = []
-
+        non_terminals = []
         for c_prod in compressed_prods:
             for prod in c_prod[1:]:
                 r = prod(c_prod[0], prod)
--- Production.py
+++ Production.py
@@ -3,22 +3,22 @@ from functools import reduce
 
 
 class Production:
-    def __init__(self, none_terminal, rhss):
-        self.none_terminal = none_terminal
+    def __init__(self, non_terminal, rhss):
+        self.non_terminal = non_terminal
         self.rhss = rhss
 
     def left_factorize(self):
         left_factorized_prod = Production.left_factorize_rhses(rhs_group=self.rhss, prefix=[])
-        return [Production(self.none_terminal, left_factorized_prod[0]), ] + left_factorized_prod[1]
+        return [Production(self.non_terminal, left_factorized_prod[0]), ] + left_factorized_prod[1]
 
     # (left_factorized_rhss, new_prods)
     @staticmethod
     def left_factorize_rhses(rhs_group, prefix):
         groups = dict()
-        ebsilon_flag = False
+        epsilon_flag = False
         for rhs in rhs_group:
             if len(rhs) == 0:
-                ebsilon_flag = True
+                epsilon_flag = True
             else:
                 if not rhs[0] in groups:
                     groups[rhs[0]] = []
@@ -26,7 +26,7 @@ class Production:
 
         if len(groups) == len(rhs_group):
             return deepcopy(rhs_group), []
-        elif ebsilon_flag or len(groups) > 1:
+        elif epsilon_flag or len(groups) > 1:
             new_prods = []
             left_factorized_rhss = []
             new_prod_name = reduce(lambda str1, str2: str1 + str2, prefix)
