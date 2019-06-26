5e52bc99e6a667fd224c16f0147495fc114d84fe
Reza <rezashirkavand1997@gmail.com>
2019-06-26 22:38:46 +0430

    complete semantic routines

--- Semantics.py
+++ Semantics.py
@@ -1,4 +1,4 @@
-
+from CGrammar import ck, cs, s
 
 class SymbolTableRecord:
     def __init__(self, type, name=None):
@@ -23,7 +23,9 @@ class Semantics:
         semantic_routine(kwargs)
 
     def scope_start(self, **kwargs):
-        self.stack.append(len(self.symbol_table))
+        current_node = kwargs.get('current_node', None)
+        if current_node is not None:
+            self.stack.append((len(self.symbol_table), current_node.non_terminal))
 
     def scope_end(self, **kwargs):
         self.symbol_table = self.symbol_table[:self.stack[-1]]
@@ -33,6 +35,7 @@ class Semantics:
         current_node = kwargs.get('current_node', None)
         if current_node is None:
             self.prev_sym_entry = None
+            return
         type = current_node.children[0]
         entry = SymbolTableRecord(type=type)
         self.symbol_table.append(entry)
@@ -53,8 +56,31 @@ class Semantics:
         param = kwargs.get('current_node', None)
         if self.prev_sym_entry is not None and param is not None:
             if 'param-len' in self.symbol_table[-1].attributes:
-                self.symbol_table[-1].attributes += 1
+                if self.symbol_table[-1].attributes["param-len"] == 0:
+                    pass
+                    #TODO check error
+                elif param == ck("void") or (hasattr(param, 'children') and param.chilren[0] == ck("void")):
+                    pass
+                    #TODO check error
+                else:
+                    self.symbol_table[-1].attributes["param-len"] += 1
             else:
-                self.symbol_table[-1].attributes = 1
+                if param == ck("int") or (hasattr(param, 'children') and param.chilren[0] == ck("int")):
+                    self.symbol_table[-1].attributes["param-len"] = 1
+                if param == ck("void") or (hasattr(param, 'children') and param.chilren[0] == ck("void")):
+                    self.symbol_table[-1].attributes["param-len"] = 0
                 self.symbol_table[-1].attributes['dec-type'] = "function"
 
+    def check_break(self):
+        for scope in self.stack[::-1]:
+            if scope[1] == "iteration_stmt" or scope[1] == "switch-stmt":
+                return True
+        return False
+
+    def check_main(self):
+        if self.symbol_table[-1].type != ck("void") or self.symbol_table[-1].name != "main" \
+                or "dec-type" not in self.symbol_table[-1].attributes \
+                or self.symbol_table[-1].attribures["dec-type"] != "function" \
+                or self.symbol_table[-1].attributes["param-len"] != 0:
+            return False
+        return True
