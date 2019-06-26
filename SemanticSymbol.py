5a4264cee96b1d8441da994e8f729b9bba76de32
svafaiet <svafaiet@gmail.com>
2019-06-26 13:06:36 +0430

    initialize SemanticSymbol

--- SemanticSymbol.py
+++ SemanticSymbol.py
@@ -1,2 +1,15 @@
-class SemanticSymbol:
-    pass
+from DirectiveSymbol import DirectiveSymbol
+
+
+class SemanticSymbol(DirectiveSymbol):
+    """
+    directive for semantic structure
+    semantic_handler will generate tables according to param type
+    different kinds of type are:
+        SCOPE_START
+        SCOPE_END #fixme should we have this
+        
+    """
+    def __init__(self, t):
+        self.type = t
+
